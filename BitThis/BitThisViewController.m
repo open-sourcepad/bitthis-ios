70cfbb145e6d515249db9b83ec6552d7fca5e8a6
Andy L <andyl@sourcepad.com>
2014-03-21 23:32:07 +0800

    - Add activity icon when converting

--- BitThis/BitThisViewController.m
+++ BitThis/BitThisViewController.m
@@ -36,9 +36,8 @@
     imageView.image = [UIImage imageNamed:@"test_16x16.png"];
 
     // Loading View
-    _loadingView = [[UIActivityIndicatorView alloc] init];
+    _loadingView = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhiteLarge];
     _loadingView.center = self.view.center;
-    _loadingView.activityIndicatorViewStyle = UIActivityIndicatorViewStyleWhiteLarge;
     [_loadingView startAnimating];
 }
 
@@ -53,12 +52,8 @@
     [_loadingView removeFromSuperview];
 }
 
-#pragma mark Button Actions
-- (IBAction)convertButtonAction:(id)sender
+- (void)startConversion
 {
-    // Show activity icon
-    [self.view addSubview:_loadingView];
-
     int count = IMAGE_DIMENSION;
     
     UIImage *imageToConvert = [Utility imageWithImage:imageView.image scaledToSize:CGSizeMake(IMAGE_DIMENSION, IMAGE_DIMENSION)];
@@ -68,6 +63,24 @@
     resultVC.result = result;
     [self.navigationController pushViewController:resultVC animated:YES];
     resultVC = nil;
+}
+
+#pragma mark Button Actions
+- (IBAction)convertButtonAction:(id)sender
+{
+    // Show activity icon
+    [self.view addSubview:_loadingView];
+    [self performSelector:@selector(startConversion) withObject:nil afterDelay:0.25];
+
+//    int count = IMAGE_DIMENSION;
+//    
+//    UIImage *imageToConvert = [Utility imageWithImage:imageView.image scaledToSize:CGSizeMake(IMAGE_DIMENSION, IMAGE_DIMENSION)];
+//    NSArray *result = [PixelController getRGBAsFromImage:imageToConvert atX:0 andY:0 count:count];
+//    
+//    ResultViewController *resultVC = [[ResultViewController alloc] initWithNibName:@"ResultViewController" bundle:nil];
+//    resultVC.result = result;
+//    [self.navigationController pushViewController:resultVC animated:YES];
+//    resultVC = nil;
     
 }
 
